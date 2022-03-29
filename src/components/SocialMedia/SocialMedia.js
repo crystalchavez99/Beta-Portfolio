145894ff63b1eb18b26a400cade3a56817ec36b4
Crystal <crystal120599@gmail.com>
2022-03-28 21:19:55 -0700

    social media

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -3,13 +3,13 @@ import './SocialMedia.css';
 function SocialMedia(){
     return(
         <div className='nav-social'>
-            <a>
+            <a href="https://www.linkedin.com/in/cchavez99/" target={"_blank"} rel="noreferrer">
                 <FaLinkedin className='icons'/>
             </a>
-            <a>
+            <a href="https://github.com/crystalchavez99" target={"_blank"} rel="noreferrer">
             <FaGithubSquare className='icons'/>
             </a>
-            <a>
+            <a href="https://twitter.com/chavezcrystal99" target={"_blank"} rel="noreferrer">
             <FaTwitterSquare className='icons'/>
             </a>
 
