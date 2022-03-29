5a6806bda0bf77b56d1b5539e91cf5d5a284a884
Crystal <crystal120599@gmail.com>
2022-03-28 21:22:27 -0700

    only home page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,8 +7,6 @@ function NavBar(){
             <h2>Crystal Chavez</h2>
             <div className="nav-links">
                 <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
             </div>
             <SocialMedia/>
         </div>
