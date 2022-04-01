78701651acf7e06a54c01052640570a021e919eb
Crystal <crystal120599@gmail.com>
2022-03-31 22:04:56 -0700

    implement resume button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Project from "./components/Projects/Project";
+import Resume from "./components/Resume/Resume";
 import './App.css';
 function App() {
   return (
     <div className="app">
       <NavBar />
       <Header />
+      <Resume/>
       <About />
       <Project/>
       <Contact/>
